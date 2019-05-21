a888c84f80c3d8a0d7f6936dd821811dabfe41a3
Bruno Jensen <x665.ninja@gmail.com>
2019-05-22 01:30:04 +0200

    #4 more tests

--- src/test/java/org/extension/spring/data/repository/internal/RepositorySpecificationExecutorImplTest.java
+++ src/test/java/org/extension/spring/data/repository/internal/RepositorySpecificationExecutorImplTest.java
@@ -1,6 +1,8 @@
 package org.extension.spring.data.repository.internal;
 
+import org.extension.spring.data.repository.annotations.TypedAsSqlResultSetMapping;
 import org.extension.spring.data.repository.specification.QuerySpecification;
+import org.extension.spring.data.repository.specification.TypedNativeQuerySpecification;
 import org.extension.spring.data.repository.specification.TypedQuerySpecification;
 import org.junit.Before;
 import org.junit.Test;
@@ -37,9 +39,8 @@ public class RepositorySpecificationExecutorImplTest {
         );
     }
 
-
     @Test
-    public void testFindAllWithTypedQuerySpecification() {
+    public void testFindAllWithTypedQuerySpecification_withLambda() {
 
         final String jpql = "FROM Person";
 
@@ -54,6 +55,68 @@ public class RepositorySpecificationExecutorImplTest {
         repositorySpecificationExecutor.findAll(
             (TypedQuerySpecification<Person>) () -> jpql
         );
+
+        verify(entityManager).createQuery(eq(jpql), eq(Person.class));
+        verify(typedQuery).getResultList();
+    }
+
+    @Test
+    public void testFindAllWithTypedQuerySpecification_withAnonymousClass() {
+
+        final String jpql = "FROM Person";
+
+        final TypedQuery typedQuery = mock(TypedQuery.class);
+
+        when(entityManager.createQuery(eq(jpql), eq(Person.class)))
+                .thenReturn(typedQuery);
+
+        when(typedQuery.getResultList())
+                .thenReturn(Collections.emptyList());
+
+        repositorySpecificationExecutor.findAll(new TypedQuerySpecification<Person>() {
+            @Override
+            public String query() {
+                return jpql;
+            }
+        });
+
+        verify(entityManager).createQuery(eq(jpql), eq(Person.class));
+        verify(typedQuery).getResultList();
+    }
+
+    @Test
+    public void testFindAllWithTypedQuerySpecification_withConcreteClass() {
+
+        final String jpql = "FROM Person";
+
+        final TypedQuery typedQuery = mock(TypedQuery.class);
+
+        when(entityManager.createQuery(eq(jpql), eq(Person.class)))
+                .thenReturn(typedQuery);
+
+        when(typedQuery.getResultList())
+                .thenReturn(Collections.emptyList());
+
+        repositorySpecificationExecutor.findAll(new PersonSpecification(jpql));
+
+        verify(entityManager).createQuery(eq(jpql), eq(Person.class));
+        verify(typedQuery).getResultList();
+    }
+
+    @Test
+    public void testFindAllWithTypedQuerySpecification_with2ndLevelConcreteClass() {
+
+        final String jpql = "FROM Person";
+
+        final TypedQuery typedQuery = mock(TypedQuery.class);
+
+        when(entityManager.createQuery(eq(jpql), eq(Person.class)))
+                .thenReturn(typedQuery);
+
+        when(typedQuery.getResultList())
+                .thenReturn(Collections.emptyList());
+
+        repositorySpecificationExecutor.findAll(new PersonSpecification2(jpql));
 
         verify(entityManager).createQuery(eq(jpql), eq(Person.class));
         verify(typedQuery).getResultList();
@@ -80,6 +143,57 @@ public class RepositorySpecificationExecutorImplTest {
         verify(query).getSingleResult();
     }
 
+    @Test
+    public void testFindAllWithTypedNativeQuerySpecification_withJPAEntity() {
+
+        final String jpql = "SELECT * FROM Person";
+
+        final Query query = mock(Query.class);
+
+        when(entityManager.createNativeQuery(eq(jpql), eq(Person.class)))
+                .thenReturn(query);
+
+        when(query.getResultList())
+                .thenReturn(Collections.emptyList());
+
+        repositorySpecificationExecutor.findAll(
+                (TypedNativeQuerySpecification<Person>) () -> jpql
+        );
+
+        verify(entityManager).createNativeQuery(eq(jpql), eq(Person.class));
+        verify(query).getResultList();
+    }
+
+    @Test
+    public void testFindAllWithTypedNativeQuerySpecification_withResultMappingPOJO() {
+
+        final String jpql = "SELECT * FROM Person";
+
+        final Query query = mock(Query.class);
+
+        when(entityManager.createNativeQuery(eq(jpql), eq("PersonResultMapping")))
+                .thenReturn(query);
+
+        when(query.getResultList())
+                .thenReturn(Collections.emptyList());
+
+        repositorySpecificationExecutor.findAll(
+                (TypedNativeQuerySpecification<PersonResultMapping>) () -> jpql, PersonResultMapping.class
+        );
+
+        verify(entityManager).createNativeQuery(eq(jpql), eq("PersonResultMapping"));
+        verify(query).getResultList();
+    }
+
+    @SqlResultSetMapping(
+        name = "PersonResultMapping",
+        entities = @EntityResult(
+            entityClass = PersonResultMapping.class,
+            fields = {
+                @FieldResult(name = "name", column = "name")
+            }
+        )
+    )
     @Entity
     public final class Person {
 
@@ -102,6 +216,43 @@ public class RepositorySpecificationExecutorImplTest {
 
         public void setName(String name) {
             this.name = name;
+        }
+    }
+
+    @TypedAsSqlResultSetMapping("PersonResultMapping")
+    static class PersonResultMapping {
+
+        private String name;
+
+        public PersonResultMapping(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private class PersonSpecification implements TypedQuerySpecification<Person> {
+
+        private String jpql;
+
+        public PersonSpecification(String jpql) {
+            this.jpql = jpql;
+        }
+
+
+        @Override
+        public String query() {
+            return jpql;
+        }
+    }
+
+
+    private class PersonSpecification2 extends PersonSpecification {
+
+        public PersonSpecification2(String jpql) {
+            super(jpql);
         }
     }
 }
